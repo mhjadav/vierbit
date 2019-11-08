2bb8d90af0baf17588564143a4e5e4d5a5235e27
nikunj <nikunj0706@gmail.com>
2019-11-08 10:05:39 +0530

    email and phone duplication for user API resolved

--- components/users/usersValidate.js
+++ components/users/usersValidate.js
@@ -5,28 +5,38 @@ const regex = require('../validation/regex')
 exports.validate = () => {
      return [ 
         body('name').exists().withMessage('name can not be empty').matches(regex.nameRegex).withMessage('Inavalid name'),
-        body('email').exists().withMessage('email can not be empty').isEmail().withMessage('Inavalid email'),
+        body('email').exists().withMessage('email can not be empty').isEmail().withMessage('Inavalid email').custom((value, {req}) => {
+          return usersModel.findOne({email : value}).then((user) => {
+            if(user){
+              if(req.params.user_id !== user.id){
+                return Promise.reject("Email already exists");
+              }
+            }
+          })
+        }),
         body('password','Invalid Password').isLength({min:6, max:20}),
-        body('phone','Invalid phone').optional().matches(regex.phoneRegex),
+        body('phone','Invalid phone').optional().matches(regex.phoneRegex).custom((value, {req}) => {
+          return usersModel.findOne({phone : value}).then((user) => {
+            if(user) {
+              if(req.params.user_id !== user.id) {
+                return Promise.reject("Phone number already exists");
+              }
+            }
+          })
+        }),
         body('gender','Please select gender').exists(),
         body('username', 'Invalid username name').exists().matches(regex.uniqueNameRegex).custom((value, {req}) => {
-          //console.log("parent : " + req.body.);
           if(req.body.parent){
-            console.log("I m in If.....");
             return usersModel.findOne({username:value, 'parent.id' : req.body.parent.id}).then((user) => {
               if(user) {
-                 // let users = users.find(user => user.parent.username === req.body.parent.username);
                 if(req.params.user_id !== user.id) {
                   return Promise.reject('Username already exists');
                 }
               }
           })
-
           } else {
             return usersModel.findOne({username:value, 'parent.id' : undefined}).then((user) => {
-              console.log("I m in ELSE.....");
               if(user) {
-                 // let users = users.find(user => user.parent.username === req.body.parent.username);
                 if(req.params.user_id !== user.id) {
                   return Promise.reject('Username already exists');
                 }
